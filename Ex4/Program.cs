1ba146e7b7653a6fa859542d23887196a148b247
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2024-12-03 08:27:57 +0100

    Strings in Const

--- Ex4/Program.cs
+++ Ex4/Program.cs
@@ -2,9 +2,46 @@
 {
     internal class Program
     {
+        const string MsgInpRectangleWidth = "Introdueix l'amplada del rectangle:";
+        const string MsgInpRectangleHeight = "Introdueix l'altura del rectangle:";
+        const string MsgRectangleArea = "L'àrea del rectangle és: ";
+        const string MsgInpCircleRadius = "Introdueix el radi del cercle:";
+        const string MsgCircumferenceCircle = "La circumferència del cercle és: ";
+        const string MsgAreaHigher = "L'àrea és més gran de 50";
+        const string MsgAreaBetween = "L'àrea és entre 20 i 50";
+        const string MsgAreaLower = "L'àrea és menor o igual a 20";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Sol·licita l'entrada de l'usuari per calcular l'àrea d'un rectangle
+            Console.WriteLine(MsgInpRectangleWidth);
+            double width = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(MsgInpRectangleHeight);
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            // Calcula l'àrea
+            double area = width * height;
+            Console.WriteLine(MsgRectangleArea + area);
+
+            // Sol·licita l'entrada de l'usuari per calcular la circumferència d'un cercle
+            Console.WriteLine(MsgInpCircleRadius);
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double circumference = 2 * Math.PI * radius;
+
+            Console.WriteLine(MsgCircumferenceCircle + circumference);
+
+            // Imprimeix un missatge basat en el valor de l'àrea
+            if (area > 50)
+            {
+                Console.WriteLine(MsgAreaHigher);
+            }
+            else if (area > 20)
+            {
+                Console.WriteLine(MsgAreaBetween);
+            }
+            else
+            {
+                Console.WriteLine(MsgAreaLower);
+            }
         }
     }
 }
